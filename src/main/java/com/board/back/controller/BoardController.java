b919c1daa43b079c76181cb9a05925abd3d5c13f
dev-jiyoung-oh <aa991102@naver.com>
2022-05-21 23:59:41 +0900

    상세보기 구현

--- src/main/java/com/board/back/controller/BoardController.java
+++ src/main/java/com/board/back/controller/BoardController.java
@@ -2,8 +2,10 @@ package com.board.back.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +38,11 @@ public class BoardController {
 	@PostMapping("/board")
 	public Board createBoard(@RequestBody Board board) {
 		return boardService.createBoard(board);
+	}
+	
+	// get board
+	@GetMapping("/board/{no}")
+	public ResponseEntity<Board> getBoardByNo(@PathVariable Integer no) {
+		return boardService.getBoard(no);
 	}
 }
